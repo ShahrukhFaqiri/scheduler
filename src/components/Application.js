0a67f8782a89f84f12aaa0456e24a78baaa3929b
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-23 17:32:29 -0700

    revamped the setDay and setInterviewer with onChange

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,7 +23,7 @@ const days = [
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
-  
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,7 +34,10 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} day={day} setDay={setDay} />
+          <DayList
+            days={days}
+            value={day}
+            setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
