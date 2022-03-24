ae80dfe95a2e8402a72fc15b22124c73ff78f534
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-24 10:19:48 -0700

    add empty and booked appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Show from "./Show";
+import Empty from "./Empty";
+import Header from "./Header";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  const timeCheck = () => {
-    if (props.time) {
-      return `Appointment at ${props.time}`;
-    }
-    return `No Appointment`;
-  };
+  // const timeCheck = () => {
+  //   if (props.time) {
+  //     return `Appointment at ${props.time}`;
+  //   }
+  //   return `No Appointment`;
+  // };
   return (
-    <article className="appointment">{timeCheck()}</article>
+    <>
+      <article className="appointment">
+        <Header time={props.time}></Header>
+        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      </article>
+    </>
   );
 }
