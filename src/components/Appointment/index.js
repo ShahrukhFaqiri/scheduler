e652dea68845d6108beaab275f0bc382b0f136ab
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-23 21:41:31 -0700

    add Empty, header, show components to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,14 @@
 import React from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   const timeCheck = () => {
-    if(props.time){
-      return `Appointment at ${props.time}`
+    if (props.time) {
+      return `Appointment at ${props.time}`;
     }
-    return `No Appointment`
-  }
+    return `No Appointment`;
+  };
   return (
-    <>
     <article className="appointment">{timeCheck()}</article>
-
-    </>
   );
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -17,7 +17,7 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{ interviewerListIndv }</ul>
+      <ul className="interviewers__list">{interviewerListIndv}</ul>
     </section>
   );
 };
