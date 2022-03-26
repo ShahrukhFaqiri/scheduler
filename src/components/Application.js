1212613e02637f3b32f1df088f939f1cabc55e02
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-26 10:27:48 -0700

    selector and application page

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,18 +1,18 @@
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 export default function Application(props) {
-  const host = `http://localhost:8001/api`;
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   });
 
+  const host = `http://localhost:8001/api`;
   useEffect(() => {
     Promise.all([
       axios.get(`${host}/days`), 
@@ -28,20 +28,16 @@ export default function Application(props) {
   }, []);
 
   const setDay = day => setState({ ...state, day });
-  const dailyAppointments = getAppointmentsForDay(state, state.day)
-  const appointmentsList = dailyAppointments.map((appointment) => {
-    if (appointment.interview) {
-      return (
-        <Appointment
-          key={appointment.id}
-          {...appointment} />
-      );
-    };
+  const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
+        interview={interview}
       />
     );
   });
@@ -67,7 +63,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentsList}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -10,3 +10,18 @@ export function getAppointmentsForDay(state, day) {
 
   return appointmentsDay;
 };
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+
+  const interviewerId = interview.interviewer;
+  const interviewerDb = state.interviewers;
+  const interviewer = interviewerDb[interviewerId]
+
+  const interviewObject = {
+    ...interview, 
+    interviewer
+  };
+  console.log(interviewObject)
+  return interviewObject;
+};
