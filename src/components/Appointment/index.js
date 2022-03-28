cb038f48c334dc54d368167dcfc724eab5193fd3
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-28 16:24:24 -0700

    remove empty array of interviewers

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -26,7 +26,7 @@ export default function Appointment(props) {
           />
         )}
        {mode === CREATE && (
-          <Form interviewers={[]} onCancel={back} />
+          <Form interviewers={props.interviewers} onCancel={back} />
         )}
       </article>
     </>
