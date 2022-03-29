72ce29514872f673b2ed417966c4896528f61850
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-28 23:08:36 -0700

    fix edit bug

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -68,6 +68,8 @@ export default function Appointment(props) {
         )}
         {mode === EDIT && (
           <Form
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.id}
             interviewers={props.interviewers}
             onCancel={back}
             onSave={save}
