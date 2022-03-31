dd10432dc3ed3752735c6b9af347fc98478a3fcb
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-30 22:26:29 -0700

    add cypress tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,12 @@
 describe("Navigation", () => {
   it("should visit root", () => {
-    cy.vist("/");
+    cy.visit("/");
   });
-})
+
+  it("should navigate to tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
+});
+});
