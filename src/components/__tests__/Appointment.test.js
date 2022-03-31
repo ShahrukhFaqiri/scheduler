ff0d0ac77c6f86ce927719736ec57086f5bb8992
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-31 14:16:22 -0700

    fix appointment test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
-import Application from "components/Application";
+import Appointment from "components/Appointment/index"
+
+afterEach(cleanup);
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
-  });
-
-  it("does something it is supposed to do", () => {
-    // ...
-  });
-
-  it("does something else it is supposed to do", () => {
-    // ...
   });
 });
